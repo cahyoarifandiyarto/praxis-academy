6c3cb924a083676cf5cee885f60425ffa37aad4b
cahyoarifandiyarto <cahyormbl470@gmail.com>
2019-11-23 13:29:38 +0700

    [UPDATED]

--- NOVICE/02-03/Kasus/README.md
+++ NOVICE/02-03/Kasus/README.md
@@ -1,11 +1,11 @@
 # Tampilan
 Tampilan pertama
+![Tampilan1](https://github.com/cahyoarifandiyarto/praxis-academy/blob/master/NOVICE/02-03/Kasus/mysample/screenshoot/WhatsApp%20Image%202019-11-20%20at%2018.50.37%20(2).jpeg)
 
-![Tampilan1](https://github.com/cahyoarifandiyarto/praxis-academy/blob/master/NOVICE/02-03/Kasus/mysample/screenshoot/WhatsApp%20Image%202019-11-20%20at%2018.50.37%20(1).jpeg)
 
 Ketika di tap 2x tampilan berubah menjadi
 
-![Tampilan2](https://github.com/cahyoarifandiyarto/praxis-academy/blob/master/NOVICE/02-03/Kasus/mysample/screenshoot/WhatsApp%20Image%202019-11-20%20at%2018.50.37%20(2).jpeg)
+![Tampilan2](https://github.com/cahyoarifandiyarto/praxis-academy/blob/master/NOVICE/02-03/Kasus/mysample/screenshoot/WhatsApp%20Image%202019-11-20%20at%2018.50.37%20(1).jpeg)
 
 Dan ketika di tap 2x tampilan berubah menjadi
 
---
+++ NOVICE/02-04/Kasus/spinner/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Spinner example',
+      debugShowCheckedModeBanner: false,
+      home: Spinner(),
+    );
+  }
+}
+
+class Spinner extends StatefulWidget {
+  @override
+  _SpinnerState createState() => _SpinnerState();
+}
+
+class _SpinnerState extends State<Spinner> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      child: Container(width: 100.0, height: 100.0, color: Colors.green),
+      builder: (BuildContext context, Widget child) {
+        return Transform.rotate(
+          angle: _controller.value * 3.0 * math.pi,
+          child: child,
+        );
+      }
+    );
+  }
+}
